dfea5da4ccf73a691e5e84076830256bec2bc198
marco2207 <drake2207@gmail.com>
2019-07-08 11:45:09 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ module.exports = function(app) {
     app.get('/about', function(req, res) {
         res.sendFile('about.html', { root: "public"});
     });
+
+    app.get('/_callback', function(req, res) {
+        res.sendFile('_callback.html', { root: "public"});
+    });
 };
