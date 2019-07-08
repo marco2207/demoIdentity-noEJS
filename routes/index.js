4c2cdabd6ddf330ae1e5c14430535c83a200fcf1
marco2207 <drake2207@gmail.com>
2019-07-08 12:55:04 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.sendFile('index.html', { root: "views"});
+        res.sendFile('index.html', { root: "view"});
     });
 
     app.get('/about', function(req, res) {
-        res.sendFile('about.html', { root: "views"});
+        res.sendFile('about.html', { root: "view"});
     });
 
     app.get('/_callback', function(req, res) {
